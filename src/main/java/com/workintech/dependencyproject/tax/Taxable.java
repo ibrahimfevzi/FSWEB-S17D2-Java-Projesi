7d51f8ed47490c31e0f7025a1cec2dba97da6d11
ibrahimfevzi <ibrahimfevzi@gmail.com>
2023-08-23 15:07:26 +0300

    done

---
+++ src/main/java/com/workintech/dependencyproject/tax/Taxable.java
@@ -0,0 +1,9 @@
+package com.workintech.dependencyproject.tax;
+
+public interface Taxable {
+
+    double getSimpleTaxRate();
+    double getMiddleTaxRate();
+    double getUpperTaxRate();
+
+}
---
+++ src/main/java/com/workintech/dependencyproject/validation/DeveloperValidation.java
@@ -0,0 +1,18 @@
+package com.workintech.dependencyproject.validation;
+
+import com.workintech.dependencyproject.mapping.DeveloperResponse;
+import com.workintech.dependencyproject.model.Developer;
+
+public class DeveloperValidation {
+
+    public static boolean isIdValid(int id){
+        return id > 0;
+    }
+
+    public static boolean isDeveloperValid(Developer developer){
+        return isIdValid(developer.getId()) &&
+                developer.getName() != null && !developer.getName().isEmpty()
+                && developer.getSalary() > 25000;
+    }
+
+}
